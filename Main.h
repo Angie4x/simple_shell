5b084fbea48d88dbfdda02e0f2a0511c167c3b9b
Angie4x <angiealdana16@icloud.com>
2022-04-13 13:31:29 -0700

    Main.h

---
+++ Main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <limits.h>
+#include <unistd.h>
+
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
+char *_strcat(char *dest, char *src);
+
+#endif
