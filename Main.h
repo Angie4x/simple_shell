949948035500924fa7e565e750edec6c55e3ef22
Angie4x <angiealdana16@icloud.com>
2022-04-13 14:05:54 -0700

    Main.h

--- Main.h
+++ Main.h
@@ -11,5 +11,12 @@ int _strlen(char *s);
 char *_strcpy(char *dest, char *src);
 int _strcmp(char *s1, char *s2);
 char *_strcat(char *dest, char *src);
+int _clear(char **args);
+int _cd(char **args);
+int _help(char **args);
+int _exit(char **args);
+int _ls(char **args);
+int _pause(char **args);
+int _echo(char **args);
 
 #endif
