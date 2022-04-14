6b8be934bc0ca8d0b76c0aee84a1494fb232d9f8
root <capdaniel_98@outlook.com>
2022-04-13 19:15:25 -0700

    add new prototypes

--- Main.h
+++ Main.h
@@ -17,5 +17,17 @@ int _help(char **args);
 int function_exit(char **args);
 int _ls(char **args);
 int _echo(char **args);
+int command(char **args);
+char *lecture(void);
+int program_threads(char **args);
+int _echo(char **args);
+int _ls(char **args);
+int function_exit(char **args);
+int _help(char **args);
+int _cd(char **args);
+int length_array_bit(void);
+int (array_function[])(char *);
+char (*array_info[]);
+char (*array_str[]);
 
 #endif
